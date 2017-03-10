a3d19979d39388c66c9921cbab1442c29800408c
StasPtichka <lenny.bonus111@gmail.com>
2017-03-10 21:21:07 +0300

    new_live

--- homePage.js
+++ homePage.js
@@ -1,91 +1,64 @@
-/**
- * Created by stas on 24.02.17.
- */
-// var config = {
-//     apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
-//     authDomain: "simpleservice-778ef.firebaseapp.com",
-//     databaseURL: "https://simpleservice-778ef.firebaseio.com",
-//     storageBucket: "simpleservice-778ef.appspot.com",
-//     messagingSenderId: "698110095322"
-// };
-// firebase.initializeApp(config);
-
-var config = {
-    apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
-    authDomain: "simpleservice-778ef.firebaseapp.com",
-    databaseURL: "https://simpleservice-778ef.firebaseio.com",
+//Стас
+ var config = {
+     apiKey: "AIzaSyB5Wgo5RO_GI5xf6fsaSfH8OdZlx-X7lqw",
+     authDomain: "simpleservice-778ef.firebaseapp.com",
+     databaseURL: "https://simpleservice-778ef.firebaseio.com",
     storageBucket: "simpleservice-778ef.appspot.com",
     messagingSenderId: "698110095322"
-};
-firebase.initializeApp(config);
+ };
+// firebase.initializeApp(config);
 
+//Дима
+/*var config = {
+    apiKey: "AIzaSyAzreIAAimmDA0-jAiqwtQZdx9gasNKONw",
+    authDomain: "booking-48d90.firebaseapp.com",
+    databaseURL: "https://booking-48d90.firebaseio.com",
+    storageBucket: "booking-48d90.appspot.com",
+    messagingSenderId: "272973760259"
+};*/
+firebase.initializeApp(config);
 
 var appHome = angular.module('appHome', []);
 
-
-appHome.config(['$routeProvider',
-    function ($routeProvider) {
+    appHome.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/create', {
+            templateUrl: 'create.html',
+            controller: 'Create'
+        }),
         $routeProvider.when('/entry', {
             templateUrl: 'entry.html',
             controller: 'Entry'
-
-        }).when('/create', {
-            templateUrl: 'create.html',
-            controller: 'Create'
-
         }).otherwise({
             redirectTo: '/'
         });
-
     }]);
 
 appHome.controller('Entry', function ($scope, $location) {
+        $scope.buttEntry = function () {
 
-    $scope.buttEntry = function (sign) {
-
-
-        email = $scope.Email;
+        email = $scope.email;
         pass = $scope.pass;
         console.log(email, pass);
         $scope.but = '';
         $scope.pass = $scope.but;
 
+
         firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
 
             if (error.code) {
                 alert(error.message)
-            }
-            else {
-
+            } else {
                 sign = true;
             }
-
-
         });
         if (sign = true) {
             $location.path('/')
         }
-        /*Тут надо скрыть вот это:
-
-         <div ng-model = "Menu" id="personalArea">
-         <p><a href="#entry">войти</a> или <a href="#create">создать аккаунт</a></p>
-         </div>
-
-         там что то типо ng-show/ng-hide ng-if тут смотрел но не пробовал
-         https://metanit.com/web/angular/2.11.php
-         */
-        // <span class = "chek-element" ng-show = "sign">email</span>
-        
-
     }
-
 
 });
 appHome.controller('Create', function ($scope, $location) {
-
-
-    $scope.buttonReg = function () {
-
+        $scope.buttonReg = function () {
 
         email = $scope.email;
         pass = $scope.pass;
@@ -100,31 +73,11 @@ appHome.controller('Create', function ($scope, $location) {
         }
         else {
             firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
-
-
                 if (error.code) {
-
-
                     alert(error.message);
-
-
                 }
-
-
             });
-
-
             $location.path('/entry');
-
-
         }
-
-
     };
-
 });
-
-
-
-
-
